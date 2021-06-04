d57cecf7371d2254c87e2adef82060969b082249
Easycelsius <ji.sub.9411@gmail.com>
2021-06-04 14:04:41 +0900

    [add] login 기능 완성

--- triplive/src/main/java/com/triplive/security/SecurityConfig.java
+++ triplive/src/main/java/com/triplive/security/SecurityConfig.java
@@ -8,24 +8,38 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
-    
+
+	// 출처 : https://velog.io/@hellozin/Spring-Security-Form-Login-%EA%B0%84%EB%8B%A8-%EC%82%AC%EC%9A%A9-%EC%84%A4%EB%AA%85%EC%84%9C-f2jzojj8bj
 	@Override
 	public void configure(WebSecurity web) throws Exception
 	{
 		// 예를들어 이런식으로 인증할것들을 풀어주는겁니다. (주로 리소스)
-		web.ignoring().antMatchers("/css/**", "/script/**", "/");
-		web.ignoring().antMatchers("/community/**");
+		web.ignoring().antMatchers("/css/**", "/script/**", "/index.do");
+		web.ignoring().antMatchers("/fonts/**", "/images/**", "/img/**", "/js/**", "/plugins/**", "/styles/**", "/logo/**");
 	}
-	@Override
-	protected void configure(HttpSecurity http) throws Exception
-	{
-		// 여기에선 리소스외에 페이지의 인증/비인증/인증권한등을 설정하는게 좋은것 같습니다.
-		http.authorizeRequests()
-			// 어드민 권한으로만 접근할 수 있는 경로.
-			.antMatchers("/admin/**").access("ROLE_ADMIN");
 
-		// http.authorizeRequests()
-		// 	// 유저 권한으로만 접근할 수 있는 경로.
-		// 	.antMatchers("/community/write.jsp").access("User");
-	}
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+		// 앞에 선언한 것이 먼저 적용됨
+        http
+			.authorizeRequests()
+				// .antMatchers("/**").permitAll() // 전부 열어두기
+				.antMatchers("/calamity/**").permitAll()
+				.antMatchers("/community/write.do").hasRole("MEMBER") // 글 남기기 페이지는 로그인 요청
+				.antMatchers("/community/**").permitAll()
+				.antMatchers("/tripinfo/**").permitAll()
+				.antMatchers("/user/**").permitAll()
+				.antMatchers("/worldweather/**").permitAll()
+				.antMatchers("/admin").hasRole("ADMIN")
+				.anyRequest().authenticated() // 그 외는 전부 권한 필요
+				.and()
+			.formLogin()
+				.loginPage("/user/login_resist_form.do") // 로그인 페이지 호출
+				.loginProcessingUrl("user/login.do") // 실제 로그인을 진행
+				.defaultSuccessUrl("/index.do") // 로그인 성공시 보여주는 페이지
+				.permitAll()
+				.and()
+			.logout();
+    }
 }
