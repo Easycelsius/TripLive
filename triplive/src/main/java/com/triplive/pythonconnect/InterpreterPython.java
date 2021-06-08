ddc5322e463bd70474bb8089218071f93f91f931
Easycelsius <ji.sub.9411@gmail.com>
2021-06-08 16:15:55 +0900

    [add] 자이썬 적용 중

--- triplive/src/main/java/com/triplive/pythonconnect/InterpreterPython.java
+++ triplive/src/main/java/com/triplive/pythonconnect/InterpreterPython.java
@@ -1,5 +1,17 @@
 package com.triplive.pythonconnect;
 
+import org.python.util.PythonInterpreter;
+
 public class InterpreterPython {
-    
+
+    public void crawlingStart(String filename){
+        // System.setProperty("python.cahedir.skip", "true");
+        // System.setProperty("python.import.pymysql", "false");
+        PythonInterpreter interpreter = new PythonInterpreter();
+        // interpreter.execfile(filename);
+        interpreter.exec("import sys");
+        interpreter.exec("print(sys.version)");
+        interpreter.exec("print('python execute')");
+        interpreter.close();
+    }
 }
