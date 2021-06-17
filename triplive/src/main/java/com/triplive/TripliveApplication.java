97c7365a700ce8ffcb9a16a012d76cc31172db0c
Easycelsius <ji.sub.9411@gmail.com>
2021-06-17 09:29:04 +0900

    [add] 스케쥴러 설정 완료(서버 시작 후 24시간)

--- triplive/src/main/java/com/triplive/TripliveApplication.java
+++ triplive/src/main/java/com/triplive/TripliveApplication.java
@@ -4,22 +4,18 @@ import com.triplive.pythonconnect.InterpreterPython;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import lombok.extern.log4j.Log4j2;
 
 @SpringBootApplication
+@EnableScheduling
 @Log4j2
 public class TripliveApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TripliveApplication.class, args);
 		log.info("스프링부트 실행 완료");
 
-		// 실행전 pip install pymysql 설치 필요
-		log.info("파이썬 스크립트 실행");
-		InterpreterPython interpreter = new InterpreterPython();
-
-		log.info("파이썬 크롤링 실행");
-		interpreter.pythonFileStart();
 	}
 
 }
